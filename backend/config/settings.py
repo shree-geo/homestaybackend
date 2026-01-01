4fa6a424a35dc8e34c85d534282203374d6c2729
unisha-of <unishakhatiwada1@gmail.com>
2026-01-01 16:45:04 +0545

    chore(pagination): make the default  pagination limit 10

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
     'DATE_FORMAT': '%Y-%m-%d',
 }
